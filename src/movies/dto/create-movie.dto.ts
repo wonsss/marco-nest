b55204cc2f210ccfd631825e51abbfcff53bfbf7
Marco <bigdatajang@gmail.com>
2023-06-18 16:25:50 +0900

    feat: update 쿼리에 대한 unit 테스트 추가

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
+  @ApiProperty()
   @IsString()
   readonly title: string;
 
+  @ApiProperty()
   @IsNumber()
   readonly year: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsString({ each: true })
   readonly genres: string[];
